c8910c9b27ef85a8a19094b622b166128b3c2fe2
Bryan <146534660+Demonism0@users.noreply.github.com>
2023-11-01 17:48:32 +0800

    Refactored Constructors to Classes
    Changed the Book constructor to a class, and displayBooks to a static
    class method.
    createCard() method is now run for every book in the myLibrary array,
    delete and changeReadStatus method added to make the code more self
    explanatory.

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;  
+        this.addToLib();
+        Book.displayBooks();
+    }
 
-function addBookToLibrary(book) {
-    myLibrary.push(book);
-}
-let myLibrary = [];
+    addToLib() {
+        myLibrary.push(this);
+    }
 
-function displayBooks() {
-    cards.innerHTML = ''; // clears all the currently displayed books
-    for (let book of myLibrary) { // creates a card for each book
+    createCard() {
         let card = document.createElement('div');
         card.classList.add('book');
         cards.appendChild(card);
@@ -25,49 +25,60 @@ function displayBooks() {
         author.classList.add('author');
         pages.classList.add('pages');
         read.classList.add('read');
-        title.textContent = book.title;
-        author.textContent = book.author;
-        pages.textContent = book.pages;
-        read.textContent = book.read;
+        title.textContent = this.title;
+        author.textContent = this.author;
+        pages.textContent = this.pages;
+        read.textContent = this.read;
         card.appendChild(title);
         card.appendChild(author);
         card.appendChild(pages);
         card.appendChild(read);
 
-        // indexNum will be "remapped" every time displayBooks() is called
-        // displayBooks is called every time 'Delete' button is clicked so that
-        // indexNum will always correspond to the correct Array index
-        let indexNum = myLibrary.indexOf(book);
         let delButton = document.createElement('button');
         card.appendChild(delButton);
         delButton.textContent = 'Delete';
         delButton.addEventListener('click', () => {
-            myLibrary.splice(indexNum, 1);
-            displayBooks();
+            this.delete();
         });
 
         let readButton = document.createElement('button');
         card.appendChild(readButton);
-        if (book.read === true) {
+        if (this.read === true) {
             readButton.textContent = 'Unread';
-        } else if (book.read === false) {
+        } else if (this.read === false) {
             readButton.textContent = 'Read';
         }
         readButton.addEventListener('click', () => {
-            book.changeReadStatus()
+            this.changeReadStatus();
         });
     }
-}
-const cards = document.querySelector('.card-container');
 
-Book.prototype.changeReadStatus = function() {
-    if (this.read === false) {
-        this.read = true;
-    } else if (this.read === true) {
-        this.read = false;
+    static displayBooks() {
+        cards.innerHTML = ''; // clears all the currently displayed books
+        for (let book of myLibrary) { // creates a card for each book
+            book.createCard(); // every card is re-created
+        }
     }
-    displayBooks();
+
+    delete() {
+        let indexNum = myLibrary.indexOf(this);
+        myLibrary.splice(indexNum, 1);
+        Book.displayBooks();
+    }
+
+    changeReadStatus() {
+        if (this.read === false) {
+            this.read = true;
+        } else if (this.read === true) {
+            this.read = false;
+        }
+        Book.displayBooks();
+    }
 }
+
+let myLibrary = [];
+
+const cards = document.querySelector('.card-container');
 
 const dialog = document.querySelector('dialog');
 const showButton = document.querySelector('button#new');
@@ -96,12 +107,10 @@ cfmButton.addEventListener('click', (event) => {
     } else {
         isBookRead = false;
     }
-    newBook = new Book(newBookTitle.value, newBookAuthor.value,
+    new Book(newBookTitle.value, newBookAuthor.value,
         Number(newBookPages.value), isBookRead);
-    addBookToLibrary(newBook);
     dialog.close();
     newBookTitle.value = '';
     newBookAuthor.value = '';
     newBookPages.value = '';
-    displayBooks();
 })
